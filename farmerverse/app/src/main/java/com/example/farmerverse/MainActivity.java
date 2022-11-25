ab22eaeacc20734e5d16be2312edf9973c1c0a50
Your Name <you@example.com>
2022-11-25 10:23:13 -0500

    small changes

--- farmerverse/app/src/main/java/com/example/farmerverse/MainActivity.java
+++ farmerverse/app/src/main/java/com/example/farmerverse/MainActivity.java
@@ -62,6 +62,9 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        if (id == R.id.menu_home) {
+            navController.navigateUp();
+        }
 
 
         return super.onOptionsItemSelected(item);
