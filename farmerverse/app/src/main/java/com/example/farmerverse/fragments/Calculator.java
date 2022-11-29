b998e78dd6ef857a9c3d13a3df15d6fde81898df
Samuel Anderson <you@example.com>
2022-11-29 10:18:06 -0500

    moved the calculator fragment into the fragments package

--- farmerverse/app/src/main/java/com/example/farmerverse/fragments/Calculator.java
+++ farmerverse/app/src/main/java/com/example/farmerverse/fragments/Calculator.java
@@ -1,4 +1,4 @@
-package com.example.farmerverse;
+package com.example.farmerverse.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.farmerverse.R;
 
 /**
  * A simple {@link Fragment} subclass.
