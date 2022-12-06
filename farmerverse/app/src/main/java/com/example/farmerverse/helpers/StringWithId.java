5718b52274ef39444792c1869b1254fc29688ae7
Samuel Anderson <samuel.c.anderson96@gmail.com>
2022-12-06 10:14:17 -0500

    fixed bugs while merging

--- farmerverse/app/src/main/java/com/example/farmerverse/helpers/StringWithId.java
+++ farmerverse/app/src/main/java/com/example/farmerverse/helpers/StringWithId.java
@@ -1,5 +1,4 @@
 package com.example.farmerverse.helpers;
-public class StringWithId {
 
 /**
  * This class is to help with spinners
@@ -12,17 +11,20 @@ public class StringWithId {
     private String string;
     private int id;
 
-    public StringWithId(String string, int id) {
+    public StringWithId(String string, int id)
+    {
         this.string = string;
         this.id = id;
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return string;
     }
 
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
 }
